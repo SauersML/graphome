1e3839d4163814ee8f59d06743d5eca2225f503c
ScottSauers <123714540+ScottSauers@users.noreply.github.com>
2024-11-22 20:17:17 -0600

    Update test_dsbevd.rs

--- tests/test_dsbevd.rs
+++ tests/test_dsbevd.rs
@@ -375,7 +375,7 @@ fn test_dlaed4() {
     let mut d = vec![0.0; 4];
     let mut z_out = vec![vec![0.0; 4]; 4];
     let info = dlaed4(&d1, &d2, &z, rho, &mut d, &mut z_out);
-    assert_eq!(result.is_ok())
+    assert_eq!(result.is_ok());
     assert_eq!(d.len(), 4);
     assert_eq!(z_out.len(), 4);
 }
