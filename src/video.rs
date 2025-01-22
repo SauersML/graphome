cd8c15fded40c8825d2895a3af5e84a88c1079f0
SauersML <123714540+SauersML@users.noreply.github.com>
2025-01-22 14:17:38 -0600

    Update video.rs

--- src/video.rs
+++ src/video.rs
@@ -279,10 +279,10 @@ pub fn make_video(points: &[Point3D]) -> Result<(), DisplayError> {
             .set_repeat(Repeat::Infinite)
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
 
-        // 3 hundredths of a second per frame
+        // 1 hundredth of a second per frame
         for mut raw in frame_buffers {
             let mut frame = GifFrame::from_rgba_speed(width as u16, height as u16, &mut raw, 10);
-            frame.delay = 3;
+            frame.delay = 1;
             encoder
                 .write_frame(&frame)
                 .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
